ecf18d9e36636081bc8c14160ac2ea917ef046e9
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-12-04 00:05:54 +0200

    Implemented login (basic / admin)

--- client.c
+++ client.c
@@ -1,18 +1,19 @@
 /* cliTCPIt.c - Exemplu de client TCP
-   Trimite un nume la server; primeste de la server "Hello nume".
+   Trimite un numar la server; primeste de la server numarul incrementat.
          
    Autor: Lenuta Alboaie  <adria@infoiasi.ro> (c)2009
 */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
 #include <string.h>
+#include <arpa/inet.h>
+
 
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
@@ -20,11 +21,27 @@ extern int errno;
 /* portul de conectare la server*/
 int port;
 
-int main (int argc, char *argv[]) {
-    int sd;			// descriptorul de socket
-    struct sockaddr_in server;	// structura folosita pentru conectare 
-    char msg[100];		// mesajul trimis
+void afisare_meniu_basic() {
+    printf("* Pentru adaugarea unei melodii la top introduceti `1`\n");       fflush(stdout);
+    printf("* Pentru votarea unei melodii introduceti `2`\n");                fflush(stdout);
+    printf("* Pentru a adauga un comentariu la o melodie introduceti `3`\n"); fflush(stdout);
+    printf("* Pentru afisarea topului general introduceti `4`\n");            fflush(stdout);
+    printf("* Pentru afisarea topului pe genuri introduceti `5`\n");          fflush(stdout);
+}
 
+void afisare_meniu_admin() {
+    afisare_meniu_basic();
+    printf("* Pentru stergerea unei melodii introduceti `6`\n");                         fflush(stdout);
+    printf("* Pentru restrictionarea la vot a unui utilizator introduceti `7`\n");       fflush(stdout);
+    printf("* Pentru restrictionarea de a comenta a unui utilizator introduceti `8`\n"); fflush(stdout);
+}
+
+
+int main (int argc, char *argv[]) {
+    int sd;			            // descriptorul de socket
+    struct sockaddr_in server;	// structura folosita pentru conectare 
+  	
+    // mesajul trimis
     /* exista toate argumentele in linia de comanda? */
     if (argc != 3) {
         printf ("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
@@ -43,10 +60,10 @@ int main (int argc, char *argv[]) {
     /* umplem structura folosita pentru realizarea conexiunii cu serverul */
     /* familia socket-ului */
     server.sin_family = AF_INET;
-
+  
     /* adresa IP a serverului */
     server.sin_addr.s_addr = inet_addr(argv[1]);
-
+  
     /* portul de conectare */
     server.sin_port = htons (port);
   
@@ -56,26 +73,50 @@ int main (int argc, char *argv[]) {
         return errno;
     }
 
-    /* citirea mesajului */
-    bzero (msg, 100);
-    printf ("[client]Introduceti un nume: ");
-    fflush (stdout);
-    read (0, msg, 100);
-  
-    /* trimiterea mesajului la server */
-    if (write (sd, msg, 100) <= 0) {
-        perror ("[client]Eroare la write() spre server.\n");
-        return errno;
+    char nume_utilizator[64];
+    char parola[64];
+
+    int user_id;
+    int admin_status;
+
+    printf("Introduceti nume utilizator: ");
+    scanf("%s", nume_utilizator);
+
+    printf("Introduceti parola: ");
+    scanf("%s", parola);
+
+    // trimit numele de utilizator catre server
+    if (write(sd, nume_utilizator, sizeof(nume_utilizator)) <= 0) {
+		perror("Eroare la trimitere nume_utilizator catre server!\n");
+	}
+
+    // trimit parola catre server
+    if (write(sd, parola, sizeof(parola)) <= 0) {
+        perror("Eroare la trimitere parola catre server!\n");
     }
 
-    /* citirea raspunsului dat de server (apel blocant pina cind serverul raspunde) */
-    if (read (sd, msg, 100) < 0) {
-        perror ("[client]Eroare la read() de la server.\n");
-        return errno;
+    // primesc user_id de la server
+    if (read(sd, &user_id, sizeof(int)) <= 0) {
+        perror("Eroare la primire user_id de la server!\n");
     }
 
-    /* afisam mesajul primit */
-    printf ("[client]Mesajul primit este: %s\n", msg);
+    // primesc admin_status de la server
+    if (read(sd, &admin_status, sizeof(int)) <= 0) {
+        perror("Eroare primire admin_status de la client!\n");
+    }
+
+    if (user_id == -1 && admin_status == -1) {
+        printf("Utilizator inexistent sau parola incorecta!\n");
+        fflush(stdout);
+    }
+    else {
+        if (admin_status != 1) {
+            afisare_meniu_basic();
+        }
+        else {
+            afisare_meniu_admin();
+        }
+    }
 
     /* inchidem conexiunea, am terminat */
     close (sd);
--- server.c
+++ server.c
@@ -1,7 +1,7 @@
-/* servTCPIt.c - Exemplu de server TCP iterativ
-   Asteapta un nume de la clienti; intoarce clientului sirul
-   "Hello nume".
-   
+/* servTCPConcTh2.c - Exemplu de server TCP concurent care deserveste clientii
+   prin crearea unui thread pentru fiecare client.
+   Asteapta un numar de la clienti si intoarce clientilor numarul incrementat.
+   Intoarce corect identificatorul din program al thread-ului.
    Autor: Lenuta Alboaie  <adria@infoiasi.ro> (c)2009
 */
 
@@ -13,26 +13,75 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sqlite3.h>
+
 
 /* portul folosit */
-#define PORT 2024
+#define PORT 2908
 
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
 
+typedef struct thData{
+	int idThread; //id-ul thread-ului tinut in evidenta de acest program
+	int cl; //descriptorul intors de accept
+} thData;
+
+
+sqlite3* db;
+int user_id = -1;
+int admin_status = -1;
+
+
+static void *treat(void *); /* functia executata de fiecare thread ce realizeaza comunicarea cu clientii */
+void raspunde(void *);
+
+static int login_callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    user_id = atoi(argv[0]);
+    admin_status = atoi(argv[1]);
+    return 0;
+}
+
+int autentificare_utilizator(sqlite3* db, char* nume_utilizator, char* parola) {
+   char sql_query[512] = "SELECT user_id, admin_status FROM users WHERE username = '";
+   char* mesaj_eroare;
+
+   strcat(sql_query, nume_utilizator);
+   strcat(sql_query, "'");
+   strcat(sql_query, " AND password = '");
+   strcat(sql_query, parola);
+   strcat(sql_query, "'");
+
+   int db_descriptor = sqlite3_exec(db, sql_query, login_callback, 0, &mesaj_eroare);
+}
+
 int main () {
     struct sockaddr_in server;	// structura folosita de server
     struct sockaddr_in from;	
-    char msg[100];		        // mesajul primit de la client 
-    char msgrasp[100]=" ";      // mesaj de raspuns pentru client
-    int sd;			            // descriptorul de socket 
+    int nr;		//mesajul primit de trimis la client 
+    int sd;		//descriptorul de socket 
+    int pid;
+    pthread_t th[100];    //Identificatorii thread-urilor care se vor crea
+	int i = 0;
 
+    int db_descriptor = sqlite3_open("top_music.db", &db); 
+    if (db_descriptor) {
+        perror("Eroare la conectarea cu baza de date!");
+        return 0;
+    }
+  
     /* crearea unui socket */
     if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
         perror ("[server]Eroare la socket().\n");
         return errno;
     }
 
+    /* utilizarea optiunii SO_REUSEADDR */
+    int on = 1;
+    setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+  
     /* pregatirea structurilor de date */
     bzero (&server, sizeof (server));
     bzero (&from, sizeof (from));
@@ -43,7 +92,7 @@ int main () {
   
     /* acceptam orice adresa */
     server.sin_addr.s_addr = htonl (INADDR_ANY);
-
+  
     /* utilizam un port utilizator */
     server.sin_port = htons (PORT);
   
@@ -54,58 +103,79 @@ int main () {
     }
 
     /* punem serverul sa asculte daca vin clienti sa se conecteze */
-    if (listen (sd, 5) == -1) {
+    if (listen (sd, 2) == -1) {
         perror ("[server]Eroare la listen().\n");
         return errno;
     }
 
-    /* servim in mod iterativ clientii... */
+    /* servim in mod concurent clientii...folosind thread-uri */
     while (1) {
         int client;
+        thData * td; //parametru functia executata de thread     
         int length = sizeof (from);
 
         printf ("[server]Asteptam la portul %d...\n",PORT);
         fflush (stdout);
 
         /* acceptam un client (stare blocanta pina la realizarea conexiunii) */
-        client = accept (sd, (struct sockaddr *) &from, &length);
-
-        /* eroare la acceptarea conexiunii de la un client */
-        if (client < 0) {
+        if ( (client = accept (sd, (struct sockaddr *) &from, &length)) < 0) {
 	        perror ("[server]Eroare la accept().\n");
 	        continue;
 	    }
-
-        /* s-a realizat conexiunea, se astepta mesajul */
-        bzero (msg, 100);
-        printf ("[server]Asteptam mesajul...\n");
-        fflush (stdout);
-      
-        /* citirea mesajului */
-        if (read (client, msg, 100) <= 0) {
-            perror ("[server]Eroare la read() de la client.\n");
-            close (client);	/* inchidem conexiunea cu clientul */
-            continue;		/* continuam sa ascultam */
-	    }
 	
-        printf ("[server]Mesajul a fost receptionat...%s\n", msg);
-      
-        /*pregatim mesajul de raspuns */
-        bzero(msgrasp,100);
-        strcat(msgrasp,"Hello ");
-        strcat(msgrasp,msg);
-      
-        printf("[server]Trimitem mesajul inapoi...%s\n",msgrasp);
-      
-      
-        /* returnam mesajul clientului */
-        if (write (client, msgrasp, 100) <= 0) {
-            perror ("[server]Eroare la write() catre client.\n");
-            continue;		/* continuam sa ascultam */
-        }
-        else
-	        printf ("[server]Mesajul a fost trasmis cu succes.\n");
-        /* am terminat cu acest client, inchidem conexiunea */
-        close (client);
+        /* s-a realizat conexiunea, se astepta mesajul */
+    
+	    // int idThread; //id-ul threadului
+	    // int cl; //descriptorul intors de accept
+        td=(struct thData*)malloc(sizeof(struct thData));	
+        td->idThread=i++;
+        td->cl=client;
+
+        pthread_create(&th[i], NULL, &treat, td);	      
+	}
+};			
+
+static void *treat(void * arg) {		
+	struct thData tdL; 
+	tdL= *((struct thData*)arg);	
+	printf ("[thread]- %d - Asteptam mesajul...\n", tdL.idThread);
+	fflush (stdout);		 
+	pthread_detach(pthread_self());		
+	raspunde((struct thData*)arg);
+	/* am terminat cu acest client, inchidem conexiunea */
+	close ((intptr_t)arg);
+	return(NULL);		
+};
+
+void raspunde(void *arg) {
+    int nr, i = 0;
+	struct thData tdL; 
+	tdL= *((struct thData*)arg);
+
+    char nume_utilizator[64];
+    char parola[64];
+
+    // citesc numele de utilizator de la client
+    if (read(tdL.cl, nume_utilizator, sizeof(nume_utilizator)) <= 0) {
+        perror("Eroare la primire nume_utilizator de la client!");
     }
+
+    // citesc parola de la client
+    if (read(tdL.cl, parola, sizeof(parola)) <= 0) {
+        perror("Eroare la primire parola de la client!");
+    }
+
+    autentificare_utilizator(db, nume_utilizator, parola);
+	
+    // trimit user_id catre client
+    if (write(tdL.cl, &user_id, sizeof(int)) <= 0) {
+        perror("Eroare la trimitere user_id catre client!");
+    }
+
+    // trimit admin_statis catre client
+    if (write(tdL.cl, &admin_status, sizeof(int)) <= 0) {
+        perror("Eroare la trimitere admin_status catre client!");
+    }
+
+    user_id = admin_status = -1; // resetam valorile atributelor
 }
